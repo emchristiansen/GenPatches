ac1a73b5a32b5890a956085aaa1576e33071dc91
Eric Christiansen <ericmc@google.com>
2014-06-24 17:05:32 -0700

    initial

---
+++ bin/Main.hs
@@ -0,0 +1,7 @@
+import           Data.Array.Repa hiding (extract, map)
+
+type Origin = Array U DIM3 Double
+
+-- makeSensor ::
+
+main = putStrLn "Hi"
---
+++ buildContinuous.sh
@@ -0,0 +1,8 @@
+run() {
+  #cabal clean &&
+  sh configure.sh &&
+  cabal build
+  #cabal haddock
+}
+
+while inotifywait -qq -r -e modify .; do echo "Running"; time run; echo "Done"; done
