f3346080e48b9f940f07b77328c93fedd7ccef9e
Eric Christiansen <ericmartinchristiansen@gmail.com>
2014-09-28 18:32:36 -0700

    renamed to GeneratePatches and working on it

--- bin/GeneratePatches.hs
+++ bin/GeneratePatches.hs
@@ -1,4 +1,4 @@
-import           Data.Array.Repa    hiding (extract, map, (++))
+-- import           Data.Array.Repa    hiding (extract, map, (++))
 -- import           Text.RawString.QQ
 -- import           System.Process
 -- import           Text.Printf
@@ -9,17 +9,25 @@ import           Data.Array.Repa    hiding (extract, map, (++))
 -- import           Control.Lens       hiding (index)
 -- import           GHC.Float
 -- import           Text.Parsec.String
-import RenderUtil
+-- import RenderUtil
 -- import Control.Lens
-import MCMC
-import SystemUtil
-import           Control.Monad.IO.Class  (liftIO)
-import           Database.Persist
-import           Database.Persist.Sqlite
-import           Database.Persist.TH
-import Pipes
-import qualified Pipes.Prelude as P
-import qualified Data.Sequence as Seq
+-- import MCMC
+-- import SystemUtil
+-- import           Control.Monad.IO.Class  (liftIO)
+-- import           Database.Persist
+-- import           Database.Persist.Sqlite
+-- import           Database.Persist.TH
+-- import Pipes
+-- import qualified Pipes.Prelude as P
+-- import qualified Data.Sequence as Seq
+import qualified Data.Array.Repa as DAR
+import qualified Data.Sequence as DS
+import qualified Pipes as P
+import qualified Pipes.Prelude as PP
+
+import DeepDescriptor.MSR
+import DeepDescriptor.MCMC.Iterate
+import DeepDescriptor.System
 
 -- share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
 -- Person
@@ -39,14 +47,16 @@ import qualified Data.Sequence as Seq
   -- rendering Rendering
 -- |]
 
-main :: IO()
+outputRoot = "~/t/2014_q3/mcmc"
+
+main :: IO ()
 main = do
   let
     s = Sensor
       40.0
-      (fromListUnboxed (Z :. 3) [278.0, 273.0, -800.0])
-      (fromListUnboxed (Z :. 3) [278.0, 273.0, -799.0])
-      (fromListUnboxed (Z :. 3) [0.0, 1.0, 0.0])
+      (DAR.fromListUnboxed (DAR.Z DAR.:. 3) [278.0, 273.0, -800.0])
+      (DAR.fromListUnboxed (DAR.Z DAR.:. 3) [278.0, 273.0, -799.0])
+      (DAR.fromListUnboxed (DAR.Z DAR.:. 3) [0.0, 1.0, 0.0])
       256
       3
       128
@@ -55,11 +65,11 @@ main = do
     m = Model "data/cbox" "cbox.xml"
   -- mcmc m v []
   makeDirectory outputRoot
-  makeDirectory renderingRoot
-  makeDirectory mvrRoot
+  makeDirectory $ renderingRoot outputRoot
+  makeDirectory $ mvrRoot outputRoot
 
-  let mvrs = mcmc2 m v Seq.empty
-  runEffect $ for (mvrs >-> P.take 100) (lift . saveMVR)
+  let mvrs = mcmc m v DS.empty
+  P.runEffect $ P.for (mvrs P.>-> PP.take 100) (P.lift . saveMSR)
 
   -- s' <- perturb s
   -- print s
